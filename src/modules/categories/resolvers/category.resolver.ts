1087a99357be3e0540f3b90c57191bce84dae01f
matzadra <matheuszadra@gmail.com>
2025-01-12 08:44:10 -0300

    feat: implement category module with CRUD and GraphQL integration

--- src/modules/categories/resolvers/category.resolver.ts
+++ src/modules/categories/resolvers/category.resolver.ts
@@ -1,4 +1,30 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category.model';
+import { CreateCategoryInput } from '../dtos/create-category.input';
+import { UpdateCategoryInput } from '../dtos/update-category.input';
 
-@Resolver()
-export class CategoryResolver {}
+@Resolver(() => Category)
+export class CategoryResolver {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Query(() => [Category])
+  async getAllCategories() {
+    return this.categoryService.findAll();
+  }
+
+  @Mutation(() => Category)
+  async createCategory(@Args('data') createCategoryInput: CreateCategoryInput) {
+    return this.categoryService.create(createCategoryInput);
+  }
+
+  @Mutation(() => Category)
+  async updateCategory(@Args('data') updateCategoryInput: UpdateCategoryInput) {
+    return this.categoryService.update(updateCategoryInput);
+  }
+
+  @Mutation(() => Boolean)
+  async deleteCategory(@Args('id', { type: () => Int }) id: number) {
+    return this.categoryService.delete(id);
+  }
+}
--- src/modules/users/resolvers/user.resolver.spec.ts
+++ src/modules/users/resolvers/user.resolver.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserResolver } from './user.resolver';
+import { UsersResolver } from './user.resolver';
 
 describe('UserResolver', () => {
-  let resolver: UserResolver;
+  let resolver: UsersResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserResolver],
+      providers: [UsersResolver],
     }).compile();
 
-    resolver = module.get<UserResolver>(UserResolver);
+    resolver = module.get<UsersResolver>(UsersResolver);
   });
 
   it('should be defined', () => {
